1b58fd140597e024aa77a51309bb6488d0051347
yossrimelki <yossrimelki@gmail.com>
2024-07-19 23:27:59 +0100

    adding Mongodb
    Mongodb

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
+
+// Middleware
+app.use(morgan('dev'));
+app.use(cors());
+
+// MongoDB connection
+mongoose.connect(process.env.database_uri);
+
+const db = mongoose.connection;
+db.on('error', (err) => {
+    console.log(err);
+});
+db.once('open', () => {
+    console.log('Database Connection Established!');
+});
+
+// Routes
 app.get('/', (req, res) => {
     res.send('hello from simple server :)');
 });
 
-app.listen(5000, () => {
-    console.log('app is running');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
